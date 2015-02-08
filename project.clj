bc95aaebb2dd4e69c1e5c05e3da00cc3961a869a
Edward Ross <eddington.ross@gmail.com>
2015-02-08 11:25:48 +1100

    Updated ANTLR4 to lastest version

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject mantl "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "MANTL: a place to hang your ANTLR(4)s"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.antlr/antlr4-runtime "4.3"]]
-  :plugins [[lein-antlr4 "0.1.1"]]
+                 [org.antlr/antlr4-runtime "4.5"]]
+  :plugins [[lein-antlr4 "0.1.2"]]
   :antlr-src-dir "."
   :antlr-dest-dir "gen-src"
   :antlr-options {:listener false}
