563e2d0ff55ddbd23e59f96e509d0006e65be69a
Edward Ross <eddington.ross@gmail.com>
2015-03-11 19:40:39 +1100

    Added github website to project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mantl "0.1.0-SNAPSHOT"
   :description "MANTL: a place to hang your ANTLR(4)s"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/eddingtonross/mantl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
